8382ebc411fe28ca09b604d370f13ac30ae52650
Vikas_Kumar <svikas641@gmail.com>
2020-04-22 16:06:12 +0530

    Added jwt Token

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,11 @@
 const express =  require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const config = require('config');
+
+const user = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Test Route
@@ -24,7 +29,50 @@ router.post('/',[
     const { name, email, password } = req.body;
     console.log(req.body);
     
-	res.send('User Route');
+    try {
+      // See if user exists
+    
+      let user = await User.findOne({ email });
+
+      if(user){
+        return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+    // Encrypt Password
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+
+    //Return jsonwebtokens
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtToken'),
+      {expiresIn: 3600},
+      (err, token) =>{
+        if(err) throw err;
+        res.json({ token });
+      });
+
+      res.send('User Registered');
+    } catch(e) {
+      // statements
+      console.log(e.message);
+      res.status(500).send('Server Error');
+    }
 })
 
 module.exports = router;
