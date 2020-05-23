6ba0d550da2cdd5dbcb6449f2e00611043977c54
Vikas_Kumar <svikas641@gmail.com>
2020-05-23 11:30:36 +0530

    prettier

--- middleware/email.js
+++ middleware/email.js
@@ -1,40 +1,45 @@
 const axios = require("axios");
-const config = require('config');
+const config = require("config");
 
-function sendEmail(toEmail,toName,subject,fromEmail,fromName,contentValue){
-  
+function sendEmail(
+  toEmail,
+  toName,
+  subject,
+  fromEmail,
+  fromName,
+  contentValue
+) {
   try {
     axios({
-    method: "post",
-    url: "https://api.sendgrid.com/v3/mail/send",
-    headers: {
-      Authorization:
-        `Bearer ${config.get('sgApiKey')}`
-    },
-    data: {
-      personalizations: [
-        {
-          to:[
-            {
-              email: toEmail,
-              name: toName
-            }
-          ],
-          subject: subject
-        }
-      ],
-      from: {
-        email: fromEmail,
-        name: fromName
+      method: "post",
+      url: "https://api.sendgrid.com/v3/mail/send",
+      headers: {
+        Authorization: `Bearer ${config.get("sgApiKey")}`,
       },
-      content: [{ type: "text/html", value: contentValue }]
-    }
-  });  
-  } catch(e) {
+      data: {
+        personalizations: [
+          {
+            to: [
+              {
+                email: toEmail,
+                name: toName,
+              },
+            ],
+            subject: subject,
+          },
+        ],
+        from: {
+          email: fromEmail,
+          name: fromName,
+        },
+        content: [{ type: "text/html", value: contentValue }],
+      },
+    });
+  } catch (e) {
     console.log(e);
   }
-  
-  console.log('end');
+
+  console.log("end");
 }
 
 module.exports = sendEmail;
