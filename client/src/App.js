506cbc5140cb91ce27a88d9ffc145370fecc52c5
Vikas_Kumar <svikas641@gmail.com>
2020-05-27 00:28:47 +0530

    Added private route for map

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Routes from "./components/routing/Routes";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import Map from "./components/map/Map";
 // Redux
 import { Provider } from "react-redux";
@@ -27,7 +28,7 @@ const App = () => {
         <Fragment>
           <Navbar />
           <Switch>
-            <Route exact path="/map" component={Map} />
+            <PrivateRoute exact path="/track-leads" component={Map} />
             <Route exact path="/" component={Landing} />
             <Route component={Routes} />
           </Switch>
