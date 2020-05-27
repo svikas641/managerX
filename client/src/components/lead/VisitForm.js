fbadbc097f008732a259de9582fa42bedff33ba2
Vikas_Kumar <svikas641@gmail.com>
2020-05-27 18:51:49 +0530

    Prettify

--- client/src/components/lead/VisitForm.js
+++ client/src/components/lead/VisitForm.js
@@ -1,48 +1,46 @@
-import React, { useState} from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { addVisit } from '../../actions/lead';
-
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addVisit } from "../../actions/lead";
 
 const initialState = {
-  status: '',
-  clientName: '',
-  clientEmail: '',
-  clientPhoneNumber: '',
-  commentBox: ''
+  status: "",
+  clientName: "",
+  clientEmail: "",
+  clientPhoneNumber: "",
+  commentBox: "",
 };
 
 const VisitForm = ({ leadId, addVisit }) => {
-   const [formData, setFormData] = useState(initialState);
-const {
+  const [formData, setFormData] = useState(initialState);
+  const {
     status,
     clientName,
     clientEmail,
     clientPhoneNumber,
-    commentBox
+    commentBox,
   } = formData;
 
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const onSubmit = (e) => {
-          e.preventDefault();
-          addVisit(leadId, formData );
-          console.log(formData)
-          setFormData(initialState);
-        };
+    e.preventDefault();
+    addVisit(leadId, formData);
+    setFormData(initialState);
+  };
   return (
-    <div className='post-form'>
-      <div className='bg-primary p'>
+    <div className="post-form">
+      <div className="bg-primary p">
         <h3>Add a Visit</h3>
       </div>
-      <form className='form my-1' onSubmit={onSubmit}>
-      	<select name="status" value={status} onChange={onChange}>
-            <option value="0">* Select Lead Status</option>
-            <option value="Met">Met</option>
-            <option value="Not met">Not met</option>
-          </select>
-      	<div className="form-group">
+      <form className="form my-1" onSubmit={onSubmit}>
+        <select name="status" value={status} onChange={onChange}>
+          <option value="0">* Select Lead Status</option>
+          <option value="Met">Met</option>
+          <option value="Not met">Not met</option>
+        </select>
+        <div className="form-group">
           <input
             type="text"
             placeholder="Client Name"
@@ -71,26 +69,23 @@ const {
             onChange={onChange}
             required
           />
-          </div>
+        </div>
         <textarea
-          name='commentBox'
-          cols='30'
-          rows='5'
-          placeholder='Any additional Comment'
+          name="commentBox"
+          cols="30"
+          rows="5"
+          placeholder="Any additional Comment"
           value={commentBox}
           onChange={onChange}
         />
-        <input type='submit' className='btn btn-dark my-1' value='Submit' />
+        <input type="submit" className="btn btn-dark my-1" value="Submit" />
       </form>
     </div>
   );
 };
 
 VisitForm.propTypes = {
-  addVisit: PropTypes.func.isRequired
+  addVisit: PropTypes.func.isRequired,
 };
 
-export default connect(
-  null,
-  { addVisit }
-)(VisitForm);
+export default connect(null, { addVisit })(VisitForm);
